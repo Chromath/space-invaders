12979aeccacc399297983981ef34f034b361923d
Chromath <162321531+Chromath@users.noreply.github.com>
2024-03-21 12:06:11 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+mySprite = sprites.create(assets.image("""
+    SI_PLAYER
+"""), SpriteKind.player)
+mySprite.set_position(80, 80)
---
+++ main.ts
@@ -0,0 +1,21 @@
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+let mySprite = sprites.create(assets.image`SI_PLAYER`, SpriteKind.Player)
+mySprite.setPosition(80, 80)
+mySprite.setVelocity(50, 0)
